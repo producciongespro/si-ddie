9348b5b003d86cf1e14906de9b9d49ca3fae1339
atarayas <32460358+atarayas@users.noreply.github.com>
2019-09-30 13:51:47 -0600

    consulta.jsx y registro_consulta.php arreglados

--- webservices/si-ddie/registro_consulta.php
+++ webservices/si-ddie/registro_consulta.php
@@ -1,43 +1,39 @@
 <?php
-	header('Access-Control-Allow-Origin: *');
-	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-	header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-  header("Content-Type: text/html; charset=utf-8");
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header("Content-Type: text/html; charset=utf-8");
+    // require 'funcs/conexion.php';
+    // require 'funcs/funcs.php';
+    require 'conectar.php';
+    $JSONData = file_get_contents("php://input");
+    $dataObject = json_decode($JSONData);
+
+    $errors = array();
+    $mjs = "";
   
-  include "conectar.php";
+        $usuario = $dataObject-> usuario;
+        $intervencion = $dataObject-> intervencion;
+        $solicitante = $dataObject-> solicitante;
+        $solicitud = $dataObject-> solicitud;
+        $fecha_solicitud = $dataObject-> fecha_solicitud;
+        $tema = $dataObject-> tema;
+        $respuesta = $dataObject-> respuesta;
+        $fecha_respuesta = $dataObject-> fecha_respuesta;
+        $mysqli = conectarDB();
+        //echo $respuesta;
+        $registro = mysqli_query($mysqli,"INSERT INTO consultas (id_usuario, id_intervencion, id_solicitante, id_solicitud, id_respuesta, tema, fecha_solicitud, fecha_respuesta) 
+                                                          VALUES( '$usuario', '$intervencion', '$solicitante',  '$solicitud', '$respuesta', '$tema', '$fecha_solicitud', '$fecha_respuesta')") or die ("Problemas al insertar registro".mysqli_error($mysqli));
+                if($registro > 0 )
+                {
 
-  
-	$JSONData = file_get_contents("php://input");
-  $dataObject = json_decode($JSONData);
+                    $mjs="Registro exitoso";
+                    // echo "<br><a href='index.php' >Iniciar Sesion</a>";
+                    echo json_encode(array('error'=>false,'usuario'=>$usuario,'mensaje'=>$mjs));
+                    exit;
 
-  // $dataObject = json_decode(file_get_contents("php://input"), true);
-  
-    
-    $id_usuario =      $dataObject->usuario;
-    $id_intervencion = $dataObject->intervencion;
-		$id_solicitante =  $dataObject->solicitante;
-		$id_solicitud =    $dataObject->solicitud;
-    $tema =            $dataObject->tema;
-		$id_respuesta =    $dataObject->respuesta;
-    $fecha_respuesta = $dataObject->fecha_respuesta;
-    $fecha_solicitud = $dataObject->fecha_solicitud;
-    
-
-    // $fecha_respuesta= date('y-m-d:H:i', strtotime($fecha_respuesta));
-    // $fecha_solicitud= date('Y-m-d H:i', strtotime($fecha_solicitud));
-
-
-    sleep(1);
-    $mysqli = conectarDB();
-
-    mysqli_query($mysqli,"INSERT INTO consultas (id_usuario,id_intervencion,id_solicitante,id_solicitud,id_respuesta,tema,fecha_solicitud,fecha_respuesta) VALUES                                                  ('$id_usuario','$id_intervencion','$id_solicitante','$id_solicitud','$id_respuesta','$tema','$fecha_solicitud','$fecha_respuesta')") or die ("Problemas al agregar elementos a la BD: ".mysqli_error($mysqli));
-
-
-// $mysqli = conectarDB();
-// mysqli_query(																																													
-//   $mysqli,"INSERT INTO usuarios (usuario, claveEncriptada, nombre, apellido1, apellido2, pais, provincia, fechaNacimiento,  sexo, token, tipoUsuario, activo) VALUES( '$usuario', '$claveEncriptada', '$nombre',  '$apellido1', '$apellido2', '$pais', '$provincia', '$fechaNacimiento', '$sexo', '$token', '$tipoUsuario', '$activo')") or die ("Problemas al insertar registro".mysqli_error($mysqli));
-
-    mysqli_close($mysqli);
-
-
+                    } else {
+                        $errors[] = "Error al registrar";
+                        echo json_encode(array('error'=>true, 'mensaje'=>$errors ));
+                    }       
 ?>
