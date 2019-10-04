99ffad8a6b3cf81748aa7e48c78b02fe5457806f
arkoscr <tutordejava@gmail.com>
2019-10-04 09:47:21 -0600

    actualizacion registro generico

--- webservices/si-ddie/registro_consulta.php
+++ webservices/si-ddie/registro_consulta.php
@@ -3,37 +3,32 @@
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
     header("Content-Type: text/html; charset=utf-8");
-    // require 'funcs/conexion.php';
-    // require 'funcs/funcs.php';
+
     require 'conectar.php';
     $JSONData = file_get_contents("php://input");
     $dataObject = json_decode($JSONData);
-
+   
+    $lista_campos = "";
+    $lista_valores  = "";
     $errors = array();
     $mjs = "";
-  
-        $usuario = $dataObject-> usuario;
-        $intervencion = $dataObject-> intervencion;
-        $solicitante = $dataObject-> solicitante;
-        $solicitud = $dataObject-> solicitud;
-        $fecha_solicitud = $dataObject-> fecha_solicitud;
-        $tema = $dataObject-> tema;
-        $respuesta = $dataObject-> respuesta;
-        $fecha_respuesta = $dataObject-> fecha_respuesta;
-        $mysqli = conectarDB();
-        //echo $respuesta;
-        $registro = mysqli_query($mysqli,"INSERT INTO consultas (id_usuario, id_intervencion, id_solicitante, id_solicitud, id_respuesta, tema, fecha_solicitud, fecha_respuesta) 
-                                                          VALUES( '$usuario', '$intervencion', '$solicitante',  '$solicitud', '$respuesta', '$tema', '$fecha_solicitud', '$fecha_respuesta')") or die ("Problemas al insertar registro".mysqli_error($mysqli));
-                if($registro > 0 )
-                {
-
-                    $mjs="Registro exitoso";
-                    // echo "<br><a href='index.php' >Iniciar Sesion</a>";
-                    echo json_encode(array('error'=>false,'usuario'=>$usuario,'mensaje'=>$mjs));
-                    exit;
-
-                    } else {
-                        $errors[] = "Error al registrar";
-                        echo json_encode(array('error'=>true, 'mensaje'=>$errors ));
-                    }       
+    $comando = "insert into ".$_GET['tabla_destino']." (";
+    foreach ($dataObject as $campo => $valor){
+        $lista_campos .= "$campo,";
+        $lista_valores  .= "'$valor',";
+    }
+    $lista_campos = substr($lista_campos,0,-1);
+    $lista_valores = substr($lista_valores,0,-1);
+    $comando .= $lista_campos.") values (".$lista_valores.")";
+    $mysqli = conectarDB();
+    $registro = mysqli_query($mysqli,$comando) or die ("Problemas al insertar registro".mysqli_error($mysqli));
+    if($registro > 0 )
+    {
+        $mjs="Registro exitoso";
+        echo json_encode(array('error'=>false, 'mensaje'=>$mjs));
+        exit;
+    } else {
+            $errors[] = "Error al registrar";
+            echo json_encode(array('error'=>true, 'mensaje'=>$errors ));
+        } 
 ?>
