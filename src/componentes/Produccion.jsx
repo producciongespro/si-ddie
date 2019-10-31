ec43019e7095d11c1a96905a0e7802bcb6e22911
atarayas <32460358+atarayas@users.noreply.github.com>
2019-10-31 14:55:03 -0600

    actualización producción

--- src/componentes/Produccion.jsx
+++ src/componentes/Produccion.jsx
@@ -10,6 +10,7 @@ import mostrarAlerta from './Alerta.js';
 import referenciasJson from '../data/referencias.json';
 
 import LoadingSpinner from './spinner/LoadingSpinner';
+import { objectExpression } from '@babel/types';
 
 const referencias = referenciasJson[0];
 
@@ -18,9 +19,14 @@ var producto = {
   "id_usuario" : 1,
   "cantidad"   : 0,
   "fecha"      : "0001-01-01",
-}
+  "poblacion"   : "",
+  "cantidad_beneficiarios" : 0,
+  "tema_video_divulgacion"  : "",
+  "numero_consecutivo" : 'NULL'
+},
+  poblacion = [],
+  me;
 
-var  me;
 
 class Produccion extends Component {
   constructor(props) {
@@ -36,6 +42,8 @@ class Produccion extends Component {
       alertaActiva : false,
       loading: false, 
       classSuccess : false,
+      verConsecutivo : false,
+      7 : false,
     
       immediate:true,   //estado de validación del form
       setFocusOnError:true,
@@ -49,19 +57,27 @@ class Produccion extends Component {
 
 
   obtenerJson = (tabla) => {
+    me = this;
    let url= referencias.consultageneral+"?tabla=" + tabla;
    axios.get(url)
       .then(res => {     
-        this.setState({ [tabla] : res.data  }); 
-        console.log("table", res.data);
-        if (tabla === 'productos_poblacion_meta') {
-          this.state.tipo_productos.map((item) => (
-            this.setState({arreglo_poblacion : res.data.id}, () => {console.log("arreglo poblacion", this.state.arreglo_poblacion);
-            })
-            // this.setState({ loading: true }, () => {
-          ))
+        me.setState({ [tabla] : res.data  }); 
+        const limite = res.data.length;
+        console.log("LIMITE TABLA", limite);
+        
+      if (tabla === 'productos_poblacion_meta') {
+        for (let index = 0; index < limite; index++) {
+          var obj = {"id":res.data[index].id, "checked": false }
+          poblacion.push(obj);
+          console.log("I contador", index);
         }
-      })
+        this.setState({arreglo_poblacion: poblacion}, () => {
+          console.log("STATE arreglo poblacion", this.state.arreglo_poblacion)
+        })
+      }
+            console.log("POBLACION", poblacion);
+      }
+      )
       .catch(function (error) {
         console.log("error",error)
       })
@@ -90,7 +106,7 @@ class Produccion extends Component {
   enviarDatosForm = () => {
       me = this;
       // console.log("producto", producto);
-      
+      producto.poblacion = JSON.stringify(this.state.arreglo_poblacion);
        this.setState({ loading: true }, () => {
         axios.post(referencias.guardaconsulta+"?tabla_destino=productos", producto)    
           .then(function (response) {
@@ -108,10 +124,28 @@ class Produccion extends Component {
 
   }
 
-  obtenerPoblacion = (e) => {
+  activarDatos = (e) => {
     //ojo limpiar el valor si no es 5, en caso de más de un registro
     console.log("Target obtener Poblacion", e.target.value);
-    e.target.value !== '1'?this.setState({ classSuccess: true }):this.setState({ classSuccess: false });
+    this.setState({ classSuccess: true });
+    this.setState({ verConsecutivo: false });
+    this.setState({ verTemaVideo: false });
+
+    switch (e.target.value) {
+      case '1':
+        this.setState({ classSuccess: false });
+      break;
+      case '4':
+        this.setState({ verConsecutivo: true });
+      break;      
+      case '7':
+        this.setState({ verTemaVideo: true });
+      break;      
+      default:
+        break;
+    }
+    // e.target.value !== '1'?this.setState({ classSuccess: true }):this.setState({ classSuccess: false });
+    // e.target.value === '4'?this.setState({ verConsecutivo: true }):this.setState({ verConsecutivo: false });
     this.obtenerDatosForm(e);    
   }
 
@@ -130,22 +164,51 @@ class Produccion extends Component {
       case "fecha":
         producto.fecha = e.target.value;
         break;
+      case "cantidad_beneficiarios":
+        producto.cantidad_beneficiarios = e.target.value;
+        break;
+      case "numero_consecutivo":
+        producto.numero_consecutivo = e.target.value;
+        break;
+      case "tema_video_divulgacion":
+        producto.tema_video_divulgacion = e.target.value;
+        break;
       default:
        // console.log("Opción fuera de rango");
         break;
     }
   }
 
-  // handleChangeCheck = (e) => {
-  //   let isChecked = e.target.checked;
-  //   console.log("valor del ",e.target.id," es: ",isChecked);
-    
-  //   // do whatever you want with isChecked value
-  // }
+  handleChangeCheck = (e) => {
+    let isChecked = e.target.checked;
+    console.log("valor del ",e.target.id," es: ",isChecked);
+    var dobles  = this.state.arreglo_poblacion.map(function(obj,i) {
+      var rObj = obj;
+      if (obj.id === e.target.id) {
+        
+        rObj.id = obj.id;
+        rObj.checked = isChecked;
+        return rObj;
+      // return item.checked = isChecked;
+      }
+      else {
+        rObj.id = obj.id;
+        rObj.checked = obj.checked;
+        return rObj;
+        // return item.checked;
+      }
+    });
+    console.log("dobles", dobles);
+        this.setState({arreglo_poblacion : dobles}, () => {
+      console.log("STATE arreglo poblacion", this.state.arreglo_poblacion)
+    })
+  }
 
   render() { 
     const  loading  = this.state.loading;
     const  classSuccess  = this.state.classSuccess;
+    const verConsecutivo = this.state.verConsecutivo;
+    const verTemaVideo = this.state.verTemaVideo;
     return (
       <React.Fragment>
         <ValidationForm onSubmit={this.handleSubmit} onErrorSubmit={this.handleErrorSubmit}
@@ -162,7 +225,7 @@ class Produccion extends Component {
           <hr/>
           <div className="form-group">
             <label className="font-len" htmlFor="tipo_producto">Seleccione el tipo de producto:</label>
-            <SelectGroup  key="selectproducto" defaultValue={'DEFAULT'} className="form-control" name="id_producto" required onChange={this.obtenerPoblacion} >
+            <SelectGroup  key="selectproducto" defaultValue={'DEFAULT'} className="form-control" name="id_producto" required onChange={this.activarDatos} >
               <option  disabled value="DEFAULT">Seleccione la opción</option>
               {
                   this.state.tipo_productos.map((item) => (
@@ -172,12 +235,25 @@ class Produccion extends Component {
               </SelectGroup>
             </div>
             <div className="row">
+              <div className={"col-sm-12 my-2 form-group" + (verTemaVideo? "":" d-none")}>
+                <label className="font-len" htmlFor="tema_video_divulgacion">Tema del video:</label>
+                <TextInput key="video" type="text" className="form-control" id="tema_video_divulgacion" name="tema_video_divulgacion" onChange={this.obtenerDatosForm}/>
+              </div>
+            </div>
+            <div className="row">
+              <div className={"col-sm-12 my-2 form-group" + (verConsecutivo? "":" d-none")}>
+                <label className="font-len" htmlFor="numero_consecutivo">Número consecutivo:</label>
+                <TextInput key="consecutivo" type="number" className="form-control" id="numero_consecutivo" name="numero_consecutivo" onChange={this.obtenerDatosForm}/>
+              </div>
+            </div>
+            <div className="row">
               <div className={"col-sm-12 my-2" + (classSuccess? "":" d-none")}>
+               <p className="font-len">Población meta: </p>
               {
                   this.state.productos_poblacion_meta.map((item) => (
                     <div className="pretty p-default p-curve">
-                    {/* <TextInput key={"poblacion"+ item.id} id={item.id}  name = {"poblacion"+ item.id} type="checkbox" onChange={this.handleChangeCheck}/> */}
-                    <TextInput key={"poblacion"+ item.id} id={item.id}  name = {"poblacion"+ item.id} type="checkbox"/>                    
+                    <TextInput key={"poblacion"+ item.id} id={item.id}  name = {"poblacion"+ item.id} type="checkbox" onChange={this.handleChangeCheck}/>
+                    {/* <TextInput key={"poblacion"+ item.id} id={item.id}  name = {"poblacion"+ item.id} type="checkbox"/>                     */}
                     <div className="state">
                         <label>{item.nombre}</label>
                     </div>
@@ -199,6 +275,13 @@ class Produccion extends Component {
             </div>
           </div>
           <hr/>
+
+          <div className="form-group col-sm-6">
+              <label className="font-len" htmlFor="cantidad_beneficiarios">Cantidad de beneficiarios:</label>
+              <TextInput key="beneficiario" type="number" className="form-control" id="cantidad_beneficiarios" name="cantidad_beneficiarios" onChange={this.obtenerDatosForm} required />
+            </div>
+          <hr/>
+
           <div className="row">
             <div className="col-md-6 center">
               <button className="btn btn-block btn-main"> 
